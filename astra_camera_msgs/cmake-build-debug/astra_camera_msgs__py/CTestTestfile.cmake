15be05821a6596e5db9595dd7552acef9ea84c8e
kairui.wang <kairui.wang@horizon.ai>
2023-09-27 14:19:48 +0800

    Remove cmake-build-debug folder

--- astra_camera_msgs/cmake-build-debug/astra_camera_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/toosimple/workspace/astra_ws/src/ros2_astra_camera/astra_camera_msgs/cmake-build-debug/astra_camera_msgs__py
-# Build directory: /home/toosimple/workspace/astra_ws/src/ros2_astra_camera/astra_camera_msgs/cmake-build-debug/astra_camera_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
